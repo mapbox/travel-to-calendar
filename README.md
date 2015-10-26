e9890b17d81aef1f497e4d11d0328c33d3299d36
tristen <tristen.brown@gmail.com>
2015-10-26 12:26:54 -0400

    Build out

--- README.md
+++ README.md
@@ -2,3 +2,33 @@ Travel to Calendar
 ---
 
 A Chrome extension that adds travel tickets to Google Calendar from GitHub.
+
+### Usage
+
+    |Start (MM/DD/YY)| End (MM/DD/YY) | Destination | Purpose of Travel|
+    |---|---|---|---|
+    | 11/15/15 | 11/20/15 | Washington, DC | Directions Sprint |
+
+
+### Setup instructions
+
+#### 1. Edit `manifest.json`
+
+Under _Matches_, enter the GitHub repo you want the "Add to calendar" feature.
+
+#### 2. Create a new Google project
+
+To start, you'll need to create a new project from the [Google Developers Console](https://console.developers.google.com/project/). Add _API key_ & _client ID_ credentials after creating a project by selecting, __Add credentials__.
+
+With these credentials create an exectutable file called `env.sh` in the root 
+of this project add add the following lines.
+
+    export GOOGLE_CLIENT_ID="XXXXXX"
+    export GOOGLE_API="XXXXXX"
+    export GOOGLE_CALENDAR_ID="XXXXXX" # The Calendar travel tickets should be inserted
+
+#### 3. Build the script
+
+Create a `bundle.js` file by running:
+
+    npm run build
--- index.js
+++ index.js
@@ -1,28 +1,69 @@
 'use strict';
 
-redraw();
-document.addEventListener('DOMSubtreeModified', redraw);
+var GoogleApi = require('google-client-wrapper');
+var g = new GoogleApi({
+  apiKey: process.env.GOOGLE_API,
+  calendar: process.env.GOOGLE_CALENDAR_ID,
+  scope: 'https://www.googleapis.com/auth/calendar',
+  client: process.env.GOOGLE_CLIENT_ID
+});
 
-function redraw() {
+function load() {
   var url = location.pathname.substring(1).split('/');
 
   // Bounce if this isnt a pull request.
   if (url[2] !== 'issues') return;
 
-  // TODO Parse the contents of the body to find the
-  // formated HTML table.
-
   // Bounce `.gh-header-actions` links do not exist.
   var actions = document.querySelector('.gh-header-actions');
   if (!actions) return;
 
-  var action = document.getElementById('travel-to-calendar');
-  if (!action) {
-    var saveToAction = document.createElement('button');
-    saveToAction.id = 'travel-to-calendar';
-    saveToAction.textContent = 'Save to calendar';
-    saveToAction.className = 'btn btn-sm';
-    action = saveToAction.firstChild;
-    actions.appendChild(saveToAction);
-  }
+  require('google-client-api')().then(function(gapi) {
+    gapi.client.setApiKey(process.env.GOOGLE_APIKEY);
+
+    // Parse the issue contents for the first table in the comment body
+    // and push the outcome into a `travel` array.
+    var travel = [];
+    var comment = document.querySelector('.timeline-comment-wrapper');
+    var travelTimeline = comment.querySelector('.comment-body table');
+
+    // Bounce if a table does not exist.
+    if (!travelTimeline) return;
+
+    var travelTimelineContents = travelTimeline.querySelectorAll('tbody td');
+
+    Array.prototype.forEach.call(travelTimelineContents, function(el) {
+      travel.push(el.textContent);
+    });
+
+    var calendarEntry = {
+      summary: '[' + travel[2] + '] ' + travel[3],
+      start: { date: travel[0] },
+      end: { date: travel[1] }
+    };
+
+    var action = document.getElementById('travel-to-calendar');
+    if (!action) {
+      var saveToAction = document.createElement('button');
+      saveToAction.id = 'travel-to-calendar';
+      saveToAction.textContent = 'Save to calendar';
+      saveToAction.className = 'btn btn-sm';
+      saveToAction.onClick = saveToCalendar();
+      action = saveToAction.firstChild;
+      actions.appendChild(saveToAction);
+    }
+
+    function saveToCalendar() {
+      // Create a new Google cal event.
+      g.calendarInsert(calendarEntry, function(err, res) {
+        if (err) return window.alert(err);
+        console.log('RES', res);
+        return window.alert('Added to Calendar!');
+      });
+    }
+
+  });
 }
+
+document.addEventListener('DOMSubtreeModified', load);
+load();
